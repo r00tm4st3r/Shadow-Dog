53bedd146eaa720afa910bb8caefee931bf4d22c
vonkjayden <vonkjayden@gmail.com>
2025-10-15 13:37:19 +0200

    Script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.height = 600;
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+let gamespeed = 5;
 
-const playerImage = new Image();
-playerImage.src = 'images/shadow_dog.png';
+const backgroundlayer1 = new Image();
+backgroundlayer1.src = 'layer-1.png';
+const backgroundlayer2 = new Image();
+backgroundlayer2.src = 'layer-2.png';
+const backgroundlayer3 = new Image();
+backgroundlayer3.src = 'layer-3.png';
+const backgroundlayer4 = new Image();
+backgroundlayer4.src = 'layer-4.png';
+const backgroundlayer5 = new Image();
+backgroundlayer5.src = 'layer-5.png';
 
-function animate
+function animate(){
+    ctx.drawImage(backgroundlayer4, 0, 0);
+    requestAnimationFrame(animate);
+};
+animate();
+
